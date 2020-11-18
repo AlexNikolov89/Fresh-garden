d4e7a97e0c8638bc7cb659259824f339360b62f0
Halunks <68344924+Halunks@users.noreply.github.com>
2020-11-18 19:58:04 +0100

    changed static media roots

--- frontend/src/pages/Slides.jsx
+++ frontend/src/pages/Slides.jsx
@@ -1,4 +1,10 @@
 import React, {Fragment, useState} from 'react'
+import {ButtonsContainer, Next, Page, Previous, Slide, SlidePageContainer, SlidesContainer} from "../style/Slides";
+import {ReactComponent as Logo} from '../assets/svgs/logo.svg'
+import {useHistory, useLocation} from "react-router-dom";
+import slide1 from '../assets/slides/1slide.png'
+import slide2 from '../assets/slides/2slide.png'
+import slide3 from '../assets/slides/2slide.png'
 
 const Slides = () => {
     const location = useLocation();
@@ -7,14 +13,18 @@ const Slides = () => {
 
     return (
         <Fragment>
-            <SlidesContainer>
-                {location.pathname === '/slides/1' && <Slide></Slide>}
-            </SlidesContainer>
-            <ButtonsContainer>
-                <Previous to={`/slides/${indexer}`} onClick={() => {if(indexer > 1) setIndexer(indexer - 1)}}><i className="fas fa-arrow-left"/></Previous>
-                <Page to={'/shop'}>Logo pending</Page>
-                <Next to={`/slides/${indexer}`} onClick={() => setIndexer(indexer + 1)}><i className="fas fa-arrow-right"/></Next>
-            </ButtonsContainer>
+            <SlidePageContainer>
+                <SlidesContainer>
+                    {indexer === 1 && <Slide src={slide1}/>}
+                    {indexer === 2 && <Slide src={slide2}/>}
+                    {indexer === 3 && <Slide src={slide3}/>}
+                </SlidesContainer>
+                <ButtonsContainer>
+                    <Previous onClick={() => {if(indexer > 1) setIndexer(indexer - 1)}}><i className="fas fa-arrow-left"/></Previous>
+                    <Page onClick={() => history.push('/shop')}><Logo /></Page>
+                    <Next onClick={() => setIndexer(indexer + 1)}><i className="fas fa-arrow-right"/></Next>
+                </ButtonsContainer>
+            </SlidePageContainer>
         </Fragment>
     )
 }
