c8e521b0ef9f5c80029d32a6cc6380dcd83bcfef
Halunks <68344924+Halunks@users.noreply.github.com>
2020-11-18 11:55:40 +0100

    changed static and media file paths in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,10 +146,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-files/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media-files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
